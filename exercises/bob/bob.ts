62d55b5e17faee6367876d23d0c90100e34da6cd
陈逢隽 <raychenfj@gmail.com>
2019-10-23 01:44:47 +0800

    complete bob

--- exercises/bob/bob.ts
+++ exercises/bob/bob.ts
@@ -1,6 +1,25 @@
 class Bob {
-    hey( /* Parameters go here */ ) {
-        // Your code here
+    hey(msg: string): string {
+        msg = msg.trim()
+        // empty string after replacing
+        if (!msg.replace(/\s*/g, '')) {
+            return 'Fine. Be that way!'
+        }
+
+        // contain uppercase letters, whitespace, question mark only, and end with question mark
+        if (!msg.match(/[^A-Z\s\?]+/g) && msg.endsWith('?')) {
+            return 'Calm down, I know what I\'m doing!'
+        }
+
+        if (msg.endsWith('?')) {
+            return 'Sure.'
+        }
+
+        // not contain lowercase letters and (end with ! or only contain uppercase letters and whitespace)
+        if (!msg.match(/[a-z]+/g) && (msg.endsWith('!')) || !msg.match(/[^A-Z\s]+/g)) {
+            return 'Whoa, chill out!'
+        }
+        return 'Whatever.'
     }
 }
 
