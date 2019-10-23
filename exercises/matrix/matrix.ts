b2d6d32c8d394e8b8b28439e2e5af412593024bf
陈逢隽 <raychenfj@gmail.com>
2019-10-23 20:35:07 +0800

    complete matrix

--- exercises/matrix/matrix.ts
+++ exercises/matrix/matrix.ts
@@ -1,6 +1,24 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
+    private _rows: Array<Array<number>>
+    private _columns: Array<Array<number>> = []
+    constructor(input: string) {
+        this._rows = input.split('\n').map(row => row.split(' ').map(Number))
+
+        for (let x = 0, width = this._rows[0].length; x < width; x++) {
+            const col: Array<number> = []
+            for (let y = 0, height = this._rows.length; y < height; y++) {
+                col.push(this._rows[y][x])
+            }
+            this._columns.push(col)
+        }
+    }
+
+    get rows(): Array<Array<number>> {
+        return this._rows
+    }
+
+    get columns(): Array<Array<number>> {
+        return this._columns
     }
 }
 
