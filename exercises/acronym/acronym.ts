8634bd09f75399766eda05f4634b35c3702fa0eb
raychenfj <chenfengjun@qiniu.com>
2019-10-28 17:51:00 +0800

    complete acronym

--- exercises/acronym/acronym.ts
+++ exercises/acronym/acronym.ts
@@ -1,5 +1,24 @@
 export default class Acronym {
   public static parse(phrase: string): string {
-    return ''
+    phrase = phrase.trim()
+    let acronym: string = ''
+    for (let i = 0, len = phrase.length; i < len; i++) {
+      if (i == 0) {
+        acronym += phrase[i].toLocaleUpperCase()
+        continue
+      }
+      if (phrase[i] === ' ' || phrase[i] === '-') {
+        acronym += phrase[i + 1].toLocaleUpperCase()
+        continue
+      }
+      if (phrase[i - 1] !== ' ' && phrase[i].match(/[A-Z]/g)) {
+        acronym += phrase[i].toLocaleUpperCase()
+        continue
+      }
+      if (phrase[i] === ':') {
+        break
+      }
+    }
+    return acronym
   }
 }
