6d13897abd59c2784d8187a90b865fc37f8bea73
raychenfj <chenfengjun@qiniu.com>
2019-11-19 11:41:43 +0800

    complete simple-cipher

--- exercises/simple-cipher/simple-cipher.ts
+++ exercises/simple-cipher/simple-cipher.ts
@@ -1,11 +1,43 @@
+const aCharCode = "a".charCodeAt(0);
+const zCharCode = "z".charCodeAt(0);
 class SimpleCipher {
-    encode( /* Parameters go here */ ) {
-        // Your code here
+  constructor(public key: string = "") {
+    if (!this.key) {
+      const length = 100 + Math.round(Math.random() * 100);
+      while (this.key.length < length) {
+        this.key += String.fromCharCode(
+          aCharCode + Math.floor(Math.random() * 26)
+        );
+      }
     }
+  }
 
-    decode( /* Parameters go here */ ) {
-        // Your code here
+  encode(input: string): string {
+    let output = "";
+    let keyLength = this.key.length;
+    for (let i = 0, len = input.length; i < len; i++) {
+      output += String.fromCharCode(
+        aCharCode +
+          ((input.charCodeAt(i) +
+            this.key.charCodeAt(i % keyLength) -
+            2 * aCharCode) %
+            26)
+      );
     }
+    return output;
+  }
+
+  decode(input: string): string {
+    let output = "";
+    let keyLength = this.key.length;
+    for (let i = 0, len = input.length; i < len; i++) {
+      let code = input.charCodeAt(i) - this.key.charCodeAt(i % keyLength);
+      output += String.fromCharCode(
+        code >= 0 ? aCharCode + code : zCharCode + 1 + code
+      );
+    }
+    return output;
+  }
 }
 
-export default SimpleCipher
+export default SimpleCipher;
