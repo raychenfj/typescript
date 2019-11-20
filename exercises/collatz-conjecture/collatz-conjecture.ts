aa5b888af89d5d869fd38d8cf03c427cf2964daf
raychenfj <chenfengjun@qiniu.com>
2019-11-20 17:08:58 +0800

    complete collatz-conjecture

--- exercises/collatz-conjecture/collatz-conjecture.ts
+++ exercises/collatz-conjecture/collatz-conjecture.ts
@@ -1,7 +1,24 @@
 class CollatzConjecture {
-    static steps( /* Parameters go here */ ) {
-        // Your code here
+  static steps(n: number) {
+    if (n === 0) {
+      throw new Error('Only positive numbers are allowed')
     }
+    if (n < 0) {
+      throw new Error('Only positive numbers are allowed')
+    }
+
+    let step = 0
+    while (n !== 1) {
+      if (n % 2 === 0) {
+        n = n / 2
+      } else {
+        n = 3 * n + 1
+      }
+      step++
+    }
+
+    return step
+  }
 }
 
 export default CollatzConjecture
